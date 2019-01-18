b9d64d143d0f68ba9d8b0cd5e6cc5123ea2ae67d
quinoa42 <quinoa42@outlook.jp>
2019-01-18 00:04:12 -0800

    [zsh] update configs for fzf

--- shells/zsh/config/config.zsh
+++ shells/zsh/config/config.zsh
@@ -56,52 +56,57 @@ mproxy() {
         echo "USAGE: $0 <ss|unset>"
     fi
 }
+
 #######################################################################
 #                                 fzf                                 #
 #######################################################################
+[[ -f /usr/share/fzf/completion.zsh ]] && source /usr/share/fzf/completion.zsh
+[[ -f /usr/share/fzf/key-bindings.zsh ]] && source /usr/share/fzf/key-bindings.zsh
 
-# colorscheme
 _gen_fzf_default_opts() {
-  local base03="234"
-  local base02="235"
-  local base01="240"
-  local base00="241"
-  local base0="244"
-  local base1="245"
-  local base2="254"
-  local base3="230"
-  local yellow="136"
-  local orange="166"
-  local red="160"
-  local magenta="125"
-  local violet="61"
-  local blue="33"
-  local cyan="37"
-  local green="64"
+    local base03="234"
+    local base02="235"
+    local base01="240"
+    local base00="241"
+    local base0="244"
+    local base1="245"
+    local base2="254"
+    local base3="230"
+    local yellow="136"
+    local orange="166"
+    local red="160"
+    local magenta="125"
+    local violet="61"
+    local blue="33"
+    local cyan="37"
+    local green="64"
 
   # Comment and uncomment below for the light theme.
 
   # Solarized Dark color scheme for fzf
-  export FZF_DEFAULT_OPTS="
-    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue
-    --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
-  "
-  ## Solarized Light color scheme for fzf
-  #export FZF_DEFAULT_OPTS="
-  #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue
-  #  --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
-  #"
+    export FZF_DEFAULT_OPTS="
+      --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue
+      --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
+      "
+      ## Solarized Light color scheme for fzf
+          #export FZF_DEFAULT_OPTS="
+          #  --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue
+          #  --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
+          #"
 }
+
 _gen_fzf_default_opts
 
-# use ag instead of find by default (and shows hidden files)
-export FZF_DEFAULT_COMMAND='ag --hidden -g ""'
+# use rg instead of find by default (and shows hidden files)
+export FZF_DEFAULT_COMMAND='rg -S --files'
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --ansi -d 30 --preview 'bat --color=always {}' --preview-window 'wrap:hidden' --bind 'ctrl-v:toggle-preview'"
 
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # preview window for long command
-export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden --bind '?:toggle-preview'"
+
+export FZF_TMUX=1
 
 #######################################################################
 #                                 BAT                                 #
